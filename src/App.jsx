c1fc0d8d24c5eb8e5712f41d62748dd8e1ed1a2e
rust7 <rust.91@icloud.com>
2020-11-17 18:54:54 +0100

    Upload video file

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ const ffmpeg = createFFmpeg({ log: true });
 function App() {
 
 	const [ready, setReady] = useState(false);
+	const [video, setVideo] = useState();
 
 	const load = async () => {
 		await ffmpeg.load();
@@ -18,11 +19,18 @@ function App() {
 		load();
 	}, [])
 	
-	return (
+	return ready ? (
 		<div className="App">
-
-		</div>	
-	);
+			{ video && <video 
+							controls
+							width="250" 
+							src={ URL.createObjectURL(video) }>
+						</video> 
+			}
+			
+			<input type="file" onChange={ (e) => setVideo(e.target.files?.item(0)) } />
+		</div>
+	) : (<p>Loading... Please wait</p>);
 }
 
 export default App;
