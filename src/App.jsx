a1bd52f38b802496ef698955ce923eeb52c51a2c
rust7 <rust.91@icloud.com>
2020-11-17 19:23:15 +0100

    ffmpeg to gif conversion

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
 	const [ready, setReady] = useState(false);
 	const [video, setVideo] = useState();
+	const [gif, setGif] = useState();
 
 	const load = async () => {
 		await ffmpeg.load();
@@ -18,6 +19,21 @@ function App() {
 	useEffect(() => {
 		load();
 	}, [])
+
+	const convertToGif = async() => {
+		// Write to memory
+		ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+		// FFMPEG Treatment
+		await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+		// Analyse result
+		const data = ffmpeg.FS('readFile', 'out.gif');
+
+		// Create URL
+		const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+		setGif(url);
+	}
 	
 	return ready ? (
 		<div className="App">
@@ -27,8 +43,12 @@ function App() {
 							src={ URL.createObjectURL(video) }>
 						</video> 
 			}
-			
+
 			<input type="file" onChange={ (e) => setVideo(e.target.files?.item(0)) } />
+
+			<h3>Result: </h3>
+			<button onClick={convertToGif}>Start Convert</button>
+			{ gif && <img src={ gif } width="250" /> }
 		</div>
 	) : (<p>Loading... Please wait</p>);
 }
